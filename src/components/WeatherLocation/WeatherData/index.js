b868f98aaa50051ffbe42759d965cd546b9bca95
Carlos Alarcon <jchuerva@github.com>
2019-12-03 15:48:58 +0100

    video 36

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -1,22 +1,20 @@
 import React from "react";
 import { WeatherExtraInfo } from "./WeatherExtraInfo";
 import WeatherTemperature from "./WeatherTemperature";
-import {
-  CLOUD,
-  CLOUDY,
-  SUN,
-  RAIN,
-  SNOW,
-  WINDY,
-  FOG
-} from "../../../constants/weathers";
-import './styles.css';
+import "./styles.css";
 
-const WeatherData = () => (
-  <div className="weatherDataCont" >
-    <WeatherTemperature temperature={20} weatherState={RAIN} />
-    <WeatherExtraInfo humidity={88} wind={"10 m/s"} />
-  </div>
-);
+const WeatherData = ({
+  data: { temperature, weatherState, humidity, wind }
+}) => {
+  return (
+    <div className="weatherDataCont">
+      <WeatherTemperature
+        temperature={temperature}
+        weatherState={weatherState}
+      />
+      <WeatherExtraInfo humidity={humidity} wind={wind} />
+    </div>
+  );
+};
 
 export default WeatherData;
--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -2,13 +2,28 @@ import React from "react";
 import Location from "./Location";
 import WeatherData from "./WeatherData";
 import './style.css';
+import {
+  CLOUD,
+  CLOUDY,
+  SUN,
+  RAIN,
+  SNOW,
+  WINDY,
+  FOG
+} from "../../constants/weathers";
 
+const data = {
+  temperature: 5,
+  weatherState: RAIN,
+  humidity: 10,
+  wind: '10 m/s'
+}
 
 // arrow function
 const WeatherLocation = () => (
   <div className="weatherLocationCont">
     <Location city="Huelva"/>
-    <WeatherData />
+    <WeatherData data={data}/>
   </div>
 );
 
