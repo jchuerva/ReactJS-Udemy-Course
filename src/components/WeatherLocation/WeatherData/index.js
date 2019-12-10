d32ea38d0349fd3d53fe5a89a52c9ea549b633f2
Carlos Alarcon <jchuerva@github.com>
2019-12-10 17:21:55 +0100

    video 37

--- src/components/WeatherLocation/WeatherData/index.js
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { WeatherExtraInfo } from "./WeatherExtraInfo";
 import WeatherTemperature from "./WeatherTemperature";
 import "./styles.css";
+import PropTypes from 'prop-types';
 
 const WeatherData = ({
   data: { temperature, weatherState, humidity, wind }
@@ -15,6 +16,15 @@ const WeatherData = ({
       <WeatherExtraInfo humidity={humidity} wind={wind} />
     </div>
   );
+};
+
+WeatherData.propTypes = {
+  data: PropTypes.shape({
+    temperature: PropTypes.number.isRequired,
+    weatherState: PropTypes.string.isRequired,
+    humidity: PropTypes.number.isRequired,
+    wind: PropTypes.string.isRequired,
+  }),
 };
 
 export default WeatherData;
