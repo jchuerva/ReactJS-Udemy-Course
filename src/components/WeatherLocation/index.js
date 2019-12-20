bae11802213d1dd36e7159ae920ce8e6b70c2e5f
Carlos Alarcon <jchuerva@github.com>
2019-12-20 18:45:06 +0100

    video39

--- src/components/WeatherLocation/index.js
+++ src/components/WeatherLocation/index.js
@@ -17,7 +17,14 @@ const data = {
   weatherState: RAIN,
   humidity: 10,
   wind: '10 m/s'
-}
+};
+
+const data2 = {
+  temperature: 10,
+  weatherState: WINDY,
+  humidity: 10,
+  wind: "10 m/s"
+};
 // functional component
 // const WeatherLocation = () => (
 //   <div className="weatherLocationCont">
@@ -28,13 +35,33 @@ const data = {
 
 // class component
 class WeatherLocation extends Component{
+
+  constructor() {
+    super();
+    this.state = {
+      city: 'Huelva',
+      data: data,
+    }
+  }
+
+  handleUpdateClick = () => {
+    console.log("actualizado");
+
+    this.setState({
+      city: 'Sevilla',
+      data: data2,
+    });
+  }
+
   render (){
+    const {city, data} = this.state;
     return (
-        <div className="weatherLocationCont">
-        <Location city="Huelva"/>
-        <WeatherData data={data}/>
+      <div className="weatherLocationCont">
+        <Location city= {city} />
+        <WeatherData data={data} />
+        <button onClick={this.handleUpdateClick}>Actualizar</button>
       </div>
-    )
+    );
   }
 };
 
