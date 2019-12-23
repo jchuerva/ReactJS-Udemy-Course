04a6d55edc237333d281b6368591e03ca07ceac6
Carlos Alarcon <jchuerva@github.com>
2019-12-23 18:08:47 +0100

    video_74

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, {Component} from "react";
+import Paper from "@material-ui/core/Paper"
+import AppBar from "@material-ui/core/AppBar"
+import Typography from "@material-ui/core/Typography"
+import Toolbar from "@material-ui/core/Toolbar"
 import { Grid, Row, Col} from 'react-flexbox-grid'
 import "./App.css";
 import LocationList from "./components/LocationList";
@@ -21,7 +25,11 @@ class App extends Component {
     return (
       <Grid className="App" >
         <Row>
-          Titulo
+          <AppBar position='sticky'>
+            <Toolbar>
+              <Typography variant='title' color='inherit'>Weather App</Typography>
+            </Toolbar>
+          </AppBar>
         </Row>
         <Row>
           <Col xs={12} md={6}>
