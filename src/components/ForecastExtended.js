00aa21a4cd8d5592d23654ae2daecba38c4049a9
Carlos Alarcon <jchuerva@github.com>
2020-01-09 17:23:46 +0100

    video_90

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,14 +1,38 @@
 import React, {Component} from "react";
 import PropTypes from "prop-types";
+import ForecastItem from "./ForecastItem"
 import './style.css';
 
+const days = [
+  'Lunes',
+  'Martes',
+  'Miercoles',
+  'Jueves',
+  'Viernes',
+  'Sabado',
+  'Domingo'
+]
+
+const data = {
+  temperature: 10,
+  humidity: 10,
+  weatherState: "normal",
+  wind: "normal"
+};
+
 class ForecastExtended extends Component{
+
+  renderForecastItemDays() {
+    return days.map(day => <ForecastItem weekDay={day} hour={10} data={data}/>);
+  }
+
   render(){
     // const city = this.props.city;
     const {city} = this.props;
     return (
       <div>
-        <h2 className='forecast-title'>Pronóstico Extendido para {city} </h2>
+        <h2 className="forecast-title">Pronóstico Extendido para {city} </h2>
+        {this.renderForecastItemDays()}
       </div>
     );
   }
---
+++ src/components/ForecastItem/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import WeatherData from "./../WeatherLocation/WeatherData";
+
+const ForecastItem = ({weekDay, hour, data}) => {
+  return (
+    <div>
+      <div>{weekDay} Hora: {hour} hs</div>
+      <WeatherData data={data}/>
+    </div>
+  )
+}
+
+ForecastItem.propTypes = {
+  weekDay: PropTypes.string.isRequired,
+  hour: PropTypes.number.isRequired,
+  data: PropTypes.shape({
+    temperature: PropTypes.number.isRequired,
+    weatherState: PropTypes.string.isRequired,
+    humidity: PropTypes.number.isRequired,
+    wind: PropTypes.string.isRequired
+  })
+};
+
+export default ForecastItem
