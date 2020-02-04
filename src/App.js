f6bdc11acf7512cf85ea5961e0945e0dc05ab7e1
Carlos Alarcon <jchuerva@github.com>
2020-02-04 11:34:44 +0100

    video 122

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, {Component} from "react";
+import { connect} from "react-redux";
 import Paper from "@material-ui/core/Paper"
 import AppBar from "@material-ui/core/AppBar"
 import Typography from "@material-ui/core/Typography"
 import Toolbar from "@material-ui/core/Toolbar"
 import { Grid, Row, Col} from 'react-flexbox-grid'
-import { createStore} from 'redux'
+import PropTypes from "prop-types"
 import "./App.css";
 import LocationList from "./components/LocationList";
 import ForecastExtended from "./components/ForecastExtended";
+import { setCity } from './actions'
 
 const cities = [
   "Huelva,es",
@@ -17,7 +19,6 @@ const cities = [
   "A Coruña,es"
 ]
 
-const store = createStore(() => {}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 
 // function App() {
@@ -32,10 +33,10 @@ class App extends Component {
   handleSelectionLocation = city => {
     this.setState({city: city}) 
     // this.setState({city}) as both parameters have the same name
-    console.log(`handleSelectionLocation ${city}`);
 
-    const action = { type: "setCity", value: city };
-    store.dispatch(action);
+    console.log(`handleSelectionLocation ${city}`);
+    // throughout redux dispatch
+    this.props.dispatchSetCity(city);
   }
 
   render (){
@@ -67,4 +68,17 @@ class App extends Component {
   }
 }
 
-export default App;
+App.propTypes = {
+  dispatchSetCity: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = dispatch => (
+  {
+    // function => action creator
+    dispatchSetCity: value => dispatch(setCity(value))
+  }
+);
+
+export default connect(null, mapDispatchToProps)(App);
+// const AppConnected = connect(null, mapDispatchToProps)(App);
+// export default AppConnected;
---
+++ src/actions/index.js
@@ -0,0 +1,4 @@
+// create an action creator
+export const SET_CITY = 'SET_CITY';
+
+export const setCity = value => ({ type: SET_CITY, value });
